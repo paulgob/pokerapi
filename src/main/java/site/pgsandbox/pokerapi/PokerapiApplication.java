3e4ea45597ef1566a44489298ec87f7002704116
paulgob <pgobbe.pro@gmail.com>
2026-05-17 11:11:49 +0200

    init

---
+++ src/main/java/site/pgsandbox/pokerapi/PokerapiApplication.java
@@ -0,0 +1,13 @@
+package site.pgsandbox.pokerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/pgsandbox/pokerapi/PokerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.pgsandbox.pokerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
